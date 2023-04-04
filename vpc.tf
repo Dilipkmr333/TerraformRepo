c2695a8300c9176303ff1f79e4204a3cc7a37396
Dilipkmr333 <121297697+Dilipkmr333@users.noreply.github.com>
2023-04-04 16:01:49 +0530

    Updated Subnet cidr

--- vpc.tf
+++ vpc.tf
@@ -1,32 +1,48 @@
 resource "aws_vpc" "demo" {
-  cidr_block       = "70.70.0.0/16"
+  cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "Terraform-vpc"
+    Name = "Terraform-demo-vpc"
   }
 }
 resource "aws_subnet" "demo1" {
   vpc_id     = aws_vpc.demo.id
-  cidr_block = "70.70.1.0/28"
+  cidr_block = "10.0.0.0/24"
 
   tags = {
-    Name = "Terraform-subnet1"
+    Name = "Terraform-demo-subnet1"
   }
 }
 resource "aws_subnet" "demo2" {
   vpc_id     = aws_vpc.demo.id
-  cidr_block = "70.70.1.16/28"
+  cidr_block = "10.0.1.0/24"
 
   tags = {
-    Name = "Terraform-subnet2"
+    Name = "Terraform-demo-subnet2"
+  }
+}
+resource "aws_subnet" "demo3" {
+  vpc_id     = aws_vpc.demo.id
+  cidr_block = "10.0.50.0/24"
+
+  tags = {
+    Name = "Terraform-demo-subnet3"
+  }
+}
+resource "aws_subnet" "demo4" {
+  vpc_id     = aws_vpc.demo.id
+  cidr_block = "10.0.51.0/24"
+
+  tags = {
+    Name = "Terraform-demo-subnet4"
   }
 }
 resource "aws_internet_gateway" "massgw" {
   vpc_id = aws_vpc.demo.id
 
   tags = {
-    Name = "Terraform-IGW"
+    Name = "Terraform-demo-IGW"
   }
 }
 resource "aws_route_table" "Pu_RT" {
