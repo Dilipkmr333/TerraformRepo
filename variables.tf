df5395e7a8b5a3b58767f5cb2d50c0b0b8222c8e
Dilipkmr333 <121297697+Dilipkmr333@users.noreply.github.com>
2023-04-04 13:45:33 +0530

    Added .tf files

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+  default = "AKIAZXWFLSBHE2RF5ANN"
+  }
+variable "secret_key" {
+  default = "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"
+  }
+variable "region" {
+  default = "ap-south-1"
+  }
